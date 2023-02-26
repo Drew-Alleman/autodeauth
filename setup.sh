5e756db0e7bc4e4ddfb69c08c801a533ca4d70c5
Drew Alleman <52759042+Drew-Alleman@users.noreply.github.com>
2023-02-26 00:34:18 -0700

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
 
 echo "[*] Creating directories and installing tools..."
 
-mkdir -p /var/log/autodeauth/loot/
+sudo mkdir -p /var/log/autodeauth/loot/
 
-output=`pip3 install wifi scapy rpi.gpio 2>&1` || echo $output
-output=`apt install python3 python3-pip wireless-tools macchanger 2>&1` || echo $output
+output=`sudo pip3 install wifi scapy 2>&1` || echo $output
+output=`sudo pip3 install rpi.gpio 2>&1` || echo $output
+output=`sudo apt install python3 python3-pip wireless-tools macchanger 2>&1` || echo $output
 
 chmod +x autodeauth.py
 
-cp autodeauth.py /usr/local/bin/autodeauth
+sudo cp autodeauth.py /usr/local/bin/autodeauth
 echo "[*] Check the README for more infomation about configuring/starting the service"
 read -p "[?] Enter your WiFi interface to use with the autodeauth service: " interface
 
-cat >/etc/systemd/system/autodeauth.service <<EOL
+sudo cat >/etc/systemd/system/autodeauth.service <<EOL
 [Unit]
 Description=Auto Deauth Service
 After=multi-user.target
